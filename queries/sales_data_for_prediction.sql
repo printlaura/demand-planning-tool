6a0458b9faa0a7855a6e213f00d2c2dba7e3c1f4
Âprintlaura <lauiaies@gmail.com>
2024-08-12 18:36:17 +0200

    oos and units sold queries

--- queries/sales_data_for_prediction.sql
+++ queries/sales_data_for_prediction.sql
@@ -2,7 +2,7 @@
 WITH category AS
 (
     SELECT DISTINCT asin, category
-    FROM DWH_PROD.CORE.DIM_PRODUCT
+    FROM STREAMLIT_POC.SANDBOX.DIM_PRODUCT_VIEW
     WHERE asin = upper('{asin}')
 ),
 
@@ -13,10 +13,9 @@ sales_data AS
            region,
            asin,
            IFF(SUM(units_sold) < 0, 0, SUM(units_sold)) AS units_sold
-    FROM DATAMARTS.BRAND_MGMT.STOCK_PERFORMANCE_TEST
+    FROM STREAMLIT_POC.SANDBOX.STOCK_PERFORMANCE_TEST_VIEW
     WHERE report_date BETWEEN DATE_TRUNC('month', DATEADD('month', -6, CURRENT_DATE))
         AND LAST_DAY(DATEADD('month', -1, CURRENT_DATE))
-        AND channel = 'AMZN'
         AND asin = upper('{asin}')
         AND region = upper('{region}')
     GROUP BY year, month, region, asin
@@ -24,22 +23,22 @@ sales_data AS
 ),
 sale_price AS
 (
-    SELECT AVG(sale_price_eur) AS sale_price_eur,
+    SELECT AVG(sale_price) AS sale_price,
            asin,
            region,
            YEAR(date) AS year,
            MONTH(date) AS month
     FROM
     (
-        SELECT asin, date, region, sale_price_eur
-        FROM DATAMARTS.BRAND_MGMT.ASIN_TRACKING_DETAILED_HISTORY
+        SELECT asin, date, region, sale_price
+        FROM STREAMLIT_POC.SANDBOX.ASIN_TRACKING_DETAILED_VIEW
         WHERE asin = upper('{asin}')
           AND region = upper('{region}')
           AND date < DATE_TRUNC('month', CURRENT_DATE)
     )
     GROUP BY asin, region, year, month
 )
-SELECT a.*, b.category, d.sale_price_eur
+SELECT a.*, b.category, d.sale_price
 FROM sales_data a
 INNER JOIN category b ON a.asin = b.asin
 LEFT JOIN sale_price d ON a.asin = d.asin
@@ -50,4 +49,3 @@ LEFT JOIN sale_price d ON a.asin = d.asin
 
 -----------
 -------
-
---
+++ queries/units_sold_per_brand_region.sql
@@ -0,0 +1,14 @@
+-- units sold per month per brand/region
+
+select iff(month(report_date) > 9,
+            cast(year(report_date) as varchar) || cast(month(report_date) as varchar),
+            cast(year(report_date) as varchar) || '0' || cast(month(report_date) as varchar)
+        ) as year_month,
+        brand,
+        region,
+        iff(sum(units_sold) < 0, 0, sum(units_sold)) as units_sold
+from STREAMLIT_POC.SANDBOX.STOCK_PERFORMANCE_TEST_VIEW
+where brand = upper('{brand}')
+    and region = upper('{region}')
+group by year_month, brand, region
+order by year_month desc
