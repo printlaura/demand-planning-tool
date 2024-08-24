1eff32ea1d890ad43f22cdbdbeac520f3818787f
Âprintlaura <lauiaies@gmail.com>
2024-08-24 12:16:46 +0200

    fix date name field in sales data for prediction query

--- sales_data_for_prediction.sql
+++ sales_data_for_prediction.sql
@@ -8,16 +8,17 @@ WITH category AS
 
 sales_data AS
 (
-    SELECT year(report_date) AS year,
-           month(report_date) AS month,
+    SELECT year(date) AS year,
+           month(date) AS month,
            region,
            asin,
            IFF(SUM(units_sold) < 0, 0, SUM(units_sold)) AS units_sold
     FROM STREAMLIT_POC.SANDBOX.STOCK_PERFORMANCE_TEST_VIEW
-    WHERE report_date BETWEEN DATE_TRUNC('month', DATEADD('month', -6, CURRENT_DATE))
+    WHERE date BETWEEN DATE_TRUNC('month', DATEADD('month', -6, CURRENT_DATE))
         AND LAST_DAY(DATEADD('month', -1, CURRENT_DATE))
         AND asin = upper('{asin}')
         AND region = upper('{region}')
+        AND year(date) > 2022
     GROUP BY year, month, region, asin
     ORDER BY year DESC, month DESC
 ),
