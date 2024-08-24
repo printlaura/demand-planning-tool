38c3e992ed2e8203c998e06b659fcb966620da9c
Âprintlaura <lauiaies@gmail.com>
2024-08-24 17:07:40 +0200

    fix year sql filter in ASIN analytics & refactor render method

--- data_analytics/asin_region_report.py
+++ data_analytics/asin_region_report.py
@@ -5,7 +5,7 @@ import streamlit as st
 def filters_selection():
     asin = st.sidebar.text_input("Enter ASIN:", "")
     region = st.sidebar.selectbox("Select a region:", ["EU", "US", "UK", "JP"], index=None, placeholder="...")
-    year = st.sidebar.selectbox("Select a year:", ["2023", "2024"], index=None, placeholder="...")
+    year = st.sidebar.multiselect("Select year(s):", ["2023", "2024"], default=[])
 
     return asin, region, year
 
@@ -23,63 +23,46 @@ class AsinRegionCase(BaseAnalyticsCase):
         st.title("ASIN")
 
         asin, region, year = filters_selection()
+
+        year_filter = ""
         if year:
             year_filter = f"and year(date) in ({', '.join(map(str, year))})"
-        else:
-            year_filter = ""
 
         if not asin or not region:
             st.error("Please enter ASIN and region.")
             return
         else:
-            st.subheader("Units sold")
-            st.write(f"{asin} - {region}")
-            st.write("")
-            st.write("")
-            st.write("")
-            self.units_sold(asin, region, year_filter)
-            st.write("")
-            st.write("")
-            st.write("")
-            st.subheader("Net sales")
-            st.write(f"{asin} - {region}")
-            st.write("")
-            st.write("")
-            self.net_sales(asin, region, year_filter)
-            st.write("")
-            st.write("")
-            st.write("")
-            st.write("")
-            st.subheader("Average sale price")
-            st.write(f"{asin} - {region}")
-            st.write("")
-            st.write("")
-            self.avg_sale_price(asin, region, year_filter)
-            st.write("")
-            st.write("")
-            st.write("")
-            st.write("")
-            st.subheader("Out of Stock days")
-            st.write(f"{asin} - {region}")
-            st.write("")
-            st.write("")
-            self.oos_days(asin, region, year_filter)
+            self.display_metric("Units sold", f"{asin} - {region}", self.units_sold, asin, region, year_filter)
+            self.display_metric("Net sales", f"{asin} - {region}", self.net_sales, asin, region, year_filter)
+            self.display_metric("Average sale price", f"{asin} - {region}", self.avg_sale_price, asin, region,
+                                year_filter)
+            self.display_metric("Out of Stock days", f"{asin} - {region}", self.oos_days, asin, region, year_filter)
 
     def units_sold(self, asin, region, year_filter):
         query = self.load_sql_query(0, asin=asin, region=region, year_filter=year_filter)
         data, columns = self.run_query(query)
+        if not data or not columns:
+            st.write("No units sold data available for this ASIN and region.")
+            return
+
         df = self.data_to_df(data, columns)
         st.bar_chart(df, x="YEAR_MONTH", y="units sold")
 
     def net_sales(self, asin, region, year_filter):
         query = self.load_sql_query(1, asin=asin, region=region, year_filter=year_filter)
         data, columns = self.run_query(query)
+        if not data or not columns:
+            st.write("No sales data available for this ASIN and region.")
+            return
         df = self.data_to_df(data, columns)
         st.bar_chart(df, x="YEAR_MONTH", y="net sales in EUR")
 
     def avg_sale_price(self, asin, region, year_filter):
         query = self.load_sql_query(2, asin=asin, region=region, year_filter=year_filter)
         data, columns = self.run_query(query)
+        if not data or not columns:
+            st.write("No price data available for this ASIN and region.")
+            return
         df = self.data_to_df(data, columns)
         if df["average sale price"].isnull().any():
             st.write("No sale price available for this item.")
@@ -89,5 +72,17 @@ class AsinRegionCase(BaseAnalyticsCase):
     def oos_days(self, asin, region, year_filter):
         query = self.load_sql_query(3, asin=asin, region=region, year_filter=year_filter)
         data, columns = self.run_query(query)
+        if not data or not columns:
+            st.write("No OOS data available for this ASIN and region.")
+            return
         df = self.data_to_df(data, columns)
         st.bar_chart(df, x="YEAR_MONTH", y="total Out of Stock days")
+
+    def display_metric(self, subheader, description, method, *method_args):
+        st.subheader(subheader)
+        st.write(description)
+        st.write("")
+        st.write("")
+        method(*method_args)
+        st.write("")
+        st.write("")
