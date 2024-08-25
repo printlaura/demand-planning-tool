0abe9515854774bb07afbbccbc944a55fedc6839
Âprintlaura <lauiaies@gmail.com>
2024-08-25 11:15:04 +0200

    update test filter selection

--- tests/data_analytics/test_filters_selection.py
+++ tests/data_analytics/test_filters_selection.py
@@ -12,31 +12,20 @@ class TestFiltersSelection(unittest.TestCase):
 
         mock_selectbox.side_effect = ["US", "2023", "06"]
 
-        expected_result = ("US", "2023", "06", "202306", "June")
+        expected_result = ("US", "2023", "06",)
         actual_result = filters_selection()
 
         self.assertEqual(expected_result, actual_result)
         mock_write.assert_not_called()
 
-    @patch('streamlit.sidebar.selectbox')
-    @patch('streamlit.write')
-    def test_filters_selection_invalid_month(self, mock_write, mock_selectbox):
-
-        future_month = (datetime.now().month + 1) % 12 or 12
-        mock_selectbox.side_effect = ["US", str(datetime.now().year), f"{future_month:02}"]
-
-        result = filters_selection()
-
-        self.assertIsNone(result)
-        mock_write.assert_called_once_with("The selected month is invalid. Please select a previous date.")
 
     @patch('streamlit.sidebar.selectbox')
     @patch('streamlit.write')
-    def test_filters_selection_no_month(self, mock_write, mock_selectbox):
+    def test_filters_selection_incomplete(self, mock_write, mock_selectbox):
 
         mock_selectbox.side_effect = ["US", "2023", None]
 
         result = filters_selection()
 
-        self.assertIsNone(result)
+        assert result == (None, None, None)
         mock_write.assert_not_called()
